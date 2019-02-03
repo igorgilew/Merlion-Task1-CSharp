a121a8d42523791d40d17e195425ce4e522d90c8
igorgilew <ig.gilew2013@gmail.com>
2019-02-03 12:30:51 +0500

    Task complited! Next step is refactoring of code.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -27,19 +27,128 @@ namespace Task1
                 else return strCharCount/a + 1;
             }
         }
-        static char[,] CreateArray(int rows, int columns, string nums)
+        static string[,] CreateArray(int rows, int columns, string nums)
         {
-            var table = new char[rows, columns];
+            var table = new string[rows, columns];
             int chrIndx = 0;
-            for(int i=0; i<rows; i++)
+            for (int i = 0; i < rows; i++)
             {
-                for(int j=0; j<columns; j++)
+                for (int j = 0; j < columns; j++)
                 {
-                    if (chrIndx < nums.Length) table[i, j] = nums[chrIndx++];
+                    if (chrIndx < nums.Length) table[i, j] = nums[chrIndx++].ToString();
+                    else table[i, j] = "*";
                 }
             }
             return table;
         }
+        static bool IsConditionTrue(string first, string second)
+        {
+            if (first == "*" || second == "*") return false;
+            return (first == second || Int32.Parse(first) + Int32.Parse(second) == 10);
+        }
+        //возвращает false если не было найдено ни одного соседа
+        static bool ArrayWalk(string[,] array)
+        {
+            bool flag = false;           
+            for (int i=0; i<array.GetLength(0); i++)
+            {
+                for(int j=0; j<array.GetLength(1); j++)
+                {
+                    if(array[i, j] != "*")
+                    {
+                         if(FindNeighbourInRowAndReplace(array, i, j)) flag=true;
+                    }
+                }
+            }
+            return flag;           
+        }
+        
+        static bool GoToTheRight(string [,] array, int row, int curElemColumn)
+        {         
+            if ((curElemColumn+1)<array.GetLength(1) && array[row, curElemColumn + 1] != "*")
+            {
+                if (IsConditionTrue(array[row, curElemColumn], array[row, curElemColumn + 1]))
+                {
+                    array[row, curElemColumn] = "*";
+                    array[row, curElemColumn + 1] = "*";
+                    return true;
+                }
+            }
+            else
+            {
+                int k = 2;
+                while (curElemColumn + k < array.GetLength(1)-1 && array[row, curElemColumn + k] == "*")
+                {
+                    k++;
+                }
+                if (curElemColumn+k < array.GetLength(1) && IsConditionTrue(array[row, curElemColumn], array[row, curElemColumn + k]))
+                {
+                    array[row, curElemColumn] = "*";
+                    array[row, curElemColumn + k] = "*";
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool GoToTheDown(string [,] array, int row, int column)
+        {
+            if ((row + 1) < array.GetLength(0) && array[row+1, column] != "*")
+            {
+                if (IsConditionTrue(array[row, column], array[row+1, column]))
+                {
+                    array[row, column] = "*";
+                    array[row+1, column] = "*";
+                    return true;
+                }
+            }
+            else
+            {
+                int k = 2;
+                while (row + k < array.GetLength(0) - 1 && array[row+k, column] == "*")
+                {
+                    k++;
+                }
+                if (row + k < array.GetLength(0) && IsConditionTrue(array[row, column], array[row+k, column]))
+                {
+                    array[row, column] = "*";
+                    array[row+k, column] = "*";
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool FindNeighbourInRowAndReplace(string [,] array, int row, int curElemColumn)
+        {             
+            return GoToTheRight(array, row, curElemColumn) || GoToTheDown(array, row, curElemColumn);
+        }
+        //возвращает количество изменений в массиве
+        static int OneIteration(string [,] arr)
+        {
+            bool flag = true;
+            int count = 0;
+            while (flag)
+            {
+                //если не будет сделано ни одной замены за весь обход, то цикл прекратится
+                flag = ArrayWalk(arr);
+                if(flag)
+                {
+                    //если изменений не было, то не печатаем массив
+                    for (int i = 0; i < arr.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < arr.GetLength(1); j++)
+                        {
+                            Console.Write(arr[i, j] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    //счетчик изменений
+                    count++;
+                }              
+            }
+            return count;
+        }
+
         static void Main(string[] args)
         {
             // a - amount of columns, n - max value
@@ -49,15 +158,36 @@ namespace Task1
             int n = Int32.Parse(Console.ReadLine());
             string str = CreateString(n);
 
-            var arr = CreateArray(CalcAmountOfRows(str.Length, a), a, str);            
-            for(int i =0; i< arr.GetLength(0); i++)
+            var arr = CreateArray(CalcAmountOfRows(str.Length, a), a, str);
+            
+            var builder = new StringBuilder();
+            if (OneIteration(arr) > 0)
             {
-               for(int j=0; j<arr.GetLength(1); j++)
-               {
+                do
+                {
+                    builder.Clear();
+                    for (int i = 0; i < arr.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < arr.GetLength(1); j++)
+                        {
+                            if (arr[i, j] != "*") builder.Append(arr[i, j]);
+                        }
+                    }
+                    arr = CreateArray(CalcAmountOfRows(builder.ToString().Length, a), a, builder.ToString());
+
+                } while (OneIteration(arr) > 0);
+            }
+
+            Console.WriteLine("\nРезультат:");            
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
                     Console.Write(arr[i, j] + " ");
-               }
+                }
                 Console.WriteLine();
             }
+
             Console.ReadLine();
         }
     }
